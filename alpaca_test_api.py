882e6041189e8d5cbdbdcaee453d42908459b1c9
Sebastian Carroll <seabass25@hotmail.com>
2020-02-22 17:40:35 +0000

    Check if market is open

--- alpaca_test_api.py
+++ alpaca_test_api.py
@@ -1,8 +1,17 @@
-import alpaca_trade_api as tradeapi
+import alpaca_trade_api as alpaca
 import threading
 import time
 import datetime
 
-API_KEY = "YOUR_API_KEY_HERE"
-API_SECRET = "YOUR_API_SECRET_HERE"
+from secrets import API_KEY
+from secrets import API_SECRET
+
 APCA_API_BASE_URL = "https://paper-api.alpaca.markets"
+
+tradeapi = alpaca.REST(API_KEY, API_SECRET, APCA_API_BASE_URL, api_version='v2') # or use ENV Vars shown below
+account = tradeapi.get_account()
+tradeapi.list_positions()
+
+is_open = "is" if tradeapi.get_clock().is_open else "is not" 
+
+print("Market", is_open,"open")
